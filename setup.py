b528f8d9fcdbc1a8df639eb598ad6257573ac3e3
Jami Griffin <jamigriffin@outlook.com>
2023-12-01 20:03:31 -0600

    publish pkg 2

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
 # put the contents of your README file into the long_description
-from pathlib import Path
+
+
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+long_description = (this_directory / "README.RST").read_text()
 
 setup(
     long_description=long_description
